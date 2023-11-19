5f6d4867355a9a3e3da4def080936a3c9501e0c8
Mohamed Ramadan <123234014+Mohamed-Ramadan1@users.noreply.github.com>
2023-11-19 14:46:27 +0200

    add

---
+++ index.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("Fucing overview");
+  } else if (pathName === "/product") {
+    res.end("Fucing Products");
+  } else if (pathName === "/api") {
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+    });
+    res.end("Fucing page note founded!");
+  }
+});
+
+server.listen(3000);
